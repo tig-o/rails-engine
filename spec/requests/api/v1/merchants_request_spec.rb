a7d8435f60d76577fc7bf1a3431afcdb00379303
Thiago Silveira <thiago1992@yahoo.com>
2022-08-31 00:27:26 -0600

    Refactor: Add merchant assertions to merchant request spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,6 +8,16 @@ describe 'Merchants API' do
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body)
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants.count).to eq(7)
+    
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(Integer)
+
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a(String)
+    end
   end
 end
