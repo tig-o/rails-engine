9a957194f0c0ac7ff5c2d0e21b03461585741859
Thiago Silveira <thiago1992@yahoo.com>
2022-08-31 00:13:27 -0600

    Feature: Add merchant route + index action in merchants controller renders all merchants

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 describe 'Merchants API' do
   it 'sends a list of merchants' do
-    create_list(:merchant, 3)
+    create_list(:merchant, 7)
 
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
   end
 end
