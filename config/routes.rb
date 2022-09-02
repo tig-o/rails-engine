c75a4621925947bfad4ee035137ff56ca6a2a2ff
Thiago Silveira <thiago1992@yahoo.com>
2022-09-02 00:56:09 -0600

    Feature: Get all items API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: :merchant_items
       end
+
+      resources :items, only: [:index]
     end
   end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     name { Faker::Games::Zelda.item }
     description { Faker::Movies::HitchhikersGuideToTheGalaxy.quote }
     unit_price { Faker::Number.decimal(l_digits: 2) }
-    merchant { Faker::Number.between(1, 9) }
+    merchant_id { Faker::Number.between(1, 10) }
   end
 end
