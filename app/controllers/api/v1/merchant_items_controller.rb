361c0938473e0c292e6b10211c110d94e9df993e
Thiago Silveira <thiago1992@yahoo.com>
2022-09-01 23:05:34 -0600

    Refactor: Add sad path feature, returns 404 in merchant items controller

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::MerchantItemsController < ApplicationController
   def index
-    merchant = Merchant.find(params[:merchant_id]) #First we need to identify a merchant
-    render json: ItemSerializer.format_items(merchant.items) #At the model level, a Merchant has_many Items
+    if Item.exists?(merchant_id: params[:merchant_id])
+      merchant = Merchant.find(params[:merchant_id]) #First we need to identify a merchant
+      render json: ItemSerializer.format_items(merchant.items) #At the model level, a Merchant has_many Items
+    else
+      render json: {error: "Not Found"}, status: 404
+    end
   end
 end
