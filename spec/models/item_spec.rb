35d8379ea087d2f1c0cdb886c8b67f096d8245d1
Thiago Silveira <thiago1992@yahoo.com>
2022-09-02 04:59:21 -0600

    Test: Item model can find all items based on query

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,7 +7,13 @@ RSpec.describe Item, type: :model do
 
   describe 'model methods' do
     it 'can find all items with keyword' do
+      rei = create(:merchant, name: "R.E.I")
 
+      tent = create(:item, name: "Tent", merchant_id: rei.id)
+      mosquito_spray = create(:item, name: "Mosquito Spray", merchant_id: rei.id)
+      bear_spray = create(:item, name: "Bear Spray", merchant_id: rei.id)
+
+      expect(Item.find_items("sPrAy")).to eq([mosquito_spray, bear_spray])
     end
   end
 end
--- spec/requests/api/v1/find_spec.rb
+++ spec/requests/api/v1/find_spec.rb
@@ -31,4 +31,43 @@ RSpec.describe 'Finder' do
     get "/api/v1/merchants/find"
     expect(response.status).to eq(400)
   end
+
+  it 'can find all items based on a partial query' do
+    rei = create(:merchant, name: "R.E.I")
+
+    s_tent = create(:item, name: "2 ppl tent", merchant_id: rei.id)
+    bear_spray = create(:item, name: "Bear Spray", merchant_id: rei.id)
+    l_tent = create(:item, name: "6 ppl tent", merchant_id: rei.id)
+
+    get "/api/v1/items/find_all?name=tEnT"
+    
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)[:data]
+    
+    expect(items.first[:attributes][:name]).to eq('2 ppl tent')
+    expect(items.last[:attributes][:name]).to eq('6 ppl tent')
+  end
+
+  it 'can return empty data' do
+    rei = create(:merchant, name: "R.E.I")
+
+    s_tent = create(:item, name: "2 ppl tent", merchant_id: rei.id)
+    bear_spray = create(:item, name: "Bear Spray", merchant_id: rei.id)
+    l_tent = create(:item, name: "6 ppl tent", merchant_id: rei.id)
+
+    get "/api/v1/items/find_all?name=zZzZz"
+    
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(items).to be_empty
+    expect(items).to be_an Array
+  end
+
+  it 'can return a 400 error if no item params are passed' do
+    get "/api/v1/items/find_all"
+    expect(response.status).to eq(400)
+  end
 end
