f6eb782eca5928f01f739d4845abc01caac18a74
Thiago Silveira <thiago1992@yahoo.com>
2022-09-02 01:37:49 -0600

    Feature: Items API creates a new item with status 201

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -10,4 +10,13 @@ class Api::V1::ItemsController < ApplicationController
       render json: {error: "Not Found"}, status: 404      
     end
   end
+
+  def create
+    item = Item.create(item_params)
+    render json: ItemSerializer.format_item(item), status: 201
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
         resources :items, only: [:index], controller: :merchant_items
       end
 
-      resources :items, only: [:index, :show]
+      resources :items, only: [:index, :show, :create]
     end
   end
 end
