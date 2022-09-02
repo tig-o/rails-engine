d9c9dcf9d889feaab444dd726e8c35e8e74c06a5
Thiago Silveira <thiago1992@yahoo.com>
2022-09-02 01:13:09 -0600

    Feature: Items API can get one item and return 404 if not found

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,4 +2,12 @@ class Api::V1::ItemsController < ApplicationController
   def index
     render json: ItemSerializer.format_items(Item.all)
   end
+
+  def show
+    if Item.exists?(params[:id])
+      render json: ItemSerializer.format_item(Item.find(params[:id]))
+    else
+      render json: {error: "Not Found"}, status: 404      
+    end
+  end
 end
